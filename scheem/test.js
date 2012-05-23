3367fbaf578aa083bd3d032a0582225099745088
Owen Wang <wangfowen@gmail.com>
2012-05-22 23:49:03 -0700

    done with basic scheem parser (lesson 3 homework 1)

--- scheem/test.js
+++ scheem/test.js
@@ -7,4 +7,19 @@ var data = fs.readFileSync('scheem.peg', 'utf-8');
 // Create my parser
 var parse = PEG.buildParser(data).parse;
 // Do a test
-console.log(assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] ));
+console.log("basic test");
+assert.deepEqual( parse("(a b c)"), [["a", "b", "c"]] );
+console.log("nested expressions test");
+assert.deepEqual( parse("(* n (factorial (- n 1)))"), [["*", "n", ["factorial", ["-", "n", "1"]]]]);
+console.log("spaces at end test"); 
+assert.deepEqual( parse("(a b c  )  "), [["a", "b", "c"]] );
+console.log("spaces in middle test");
+assert.deepEqual( parse("(a  b   c )"), [["a", "b", "c"]] );
+console.log("spaces at start test");
+assert.deepEqual( parse("  (  a b c)"), [["a", "b", "c"]] );
+console.log("tabs test");
+assert.deepEqual( parse("(a \tb c)"), [["a", "b", "c"]] );
+console.log("comments test");
+assert.deepEqual( parse(";;(a b c) @#$(#$^*|\n(a b c)"), [["a", "b", "c"]] );
+console.log("multiple expressions test");
+assert.deepEqual( parse("(a b c)\n(e f g)"), [["a", "b", "c"],["e", "f", "g"]] );
